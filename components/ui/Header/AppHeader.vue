1ed88d07a6c92b074708e51ee6b0b7df3d63f090
Victor Almeida <victor.almeida@seedz.ag>
2022-04-12 01:15:41 -0300

    feat: add initial configuration of complete information

--- components/ui/Header/AppHeader.vue
+++ components/ui/Header/AppHeader.vue
@@ -3,21 +3,18 @@
     <v-toolbar>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>My Personal Project</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <a
+        href="https://www.linkedin.com/in/victor-almeida-304b05205/"
+        target="_blank"
+      >
+        <v-btn icon>
+          <v-icon> mdi-linkedin </v-icon>
+        </v-btn>
+      </a>
     </v-toolbar>
   </v-card>
 </template>
--- pages/patients/_id/index.vue
+++ pages/patients/_id/index.vue
@@ -1,32 +1,63 @@
 <template>
-  <v-card></v-card>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="justify-center">
+            <v-avatar size="256">
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+            </v-avatar>
+          </v-card-title>
+          <v-card-subtitle class="text-center">
+            Profile page of patient X
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card>
+          <v-card-title class="justify-center"> Left Card </v-card-title>
+          <v-card-subtitle class="text-center">
+            Left card info
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="8">
+        <v-card>
+          <v-card-title class="justify-center">
+            Right Card - Detailed infos
+          </v-card-title>
+          <v-card-subtitle class="text-center"> Informations </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+// import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'PatientCompleteProfile',
 
-  computed: {
-    ...mapGetters('patients', ['getPatients']),
-  },
+  // computed: {
+  //   ...mapGetters('patients', ['getPatients']),
+  // },
 
-  mounted() {
-    this.fetchPatients()
-  },
+  // mounted() {
+  //   this.fetchPatients()
+  // },
 
-  methods: {
-    ...mapActions('patients', ['findPatients']),
+  // methods: {
+  //   ...mapActions('patients', ['findPatients']),
 
-    async fetchPatients() {
-      try {
-        await this.findPatients()
-      } catch (error) {
-        console.log(error)
-      }
-    },
-  },
+  //   async fetchPatients() {
+  //     try {
+  //       await this.findPatients()
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   },
+  // },
 }
 </script>
 
